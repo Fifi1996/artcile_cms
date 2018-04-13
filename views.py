60893b5577de00038574e69d8f29e8f50a36f20d
Fifi1996 <zhuangfifi@gmail.com>
2018-04-13 19:36:18 +0800

    编写静态文件

--- views.py
+++ views.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 # 登录
 @app.route("/login/", methods=["GET", "POST]"])
 def login():
-    return render_template("login.html")  # 渲染模版
+    return render_template("login.html", title="登录")  # 渲染模版
 
 
 # 用户注册
 @app.route("/register/", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    return render_template("register.html", title="注册")
 
 
 # 用户退出（跳转到登录页面302重定向）
@@ -25,7 +25,7 @@ def logout():
 # 发布文章
 @app.route("/art/add/", methods=["GET", "POST"])
 def art_add():
-    return render_template("art_add.html")
+    return render_template("art_add.html", title="发布文章")
 
 
 # 编辑文章
@@ -43,7 +43,7 @@ def art_del(id):
 # 文章列表
 @app.route("/art/list/", methods=["GET"])
 def art_lis():
-    return render_template("art_list.html")
+    return render_template("art_list.html", title="文章列表")
 
 
 if __name__ == '__main__':
